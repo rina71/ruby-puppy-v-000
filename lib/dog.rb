81fd386890a0df794447108afa742bc3ee0503f7
Yurina Shin <yurinas@gmail.com>
2017-12-26 23:22:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,5 +7,15 @@ class Dog
     @name = name
     @@all << self
   end
+
+  def self.all
+    @@all.each do |x| puts x
+    end
     
+  end
+    
+  def self.clear_all
+    @@all.clear
+  end
+
 end
